5b8918cc99783540382b891aed5b5aaea99d50f9
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-17 14:43:36 +0300

    Minor change.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Berkay!");
 
-        int myFirstNumber = 5;
+        int myFirstNumber = 10;
         System.out.println("My first number: " + myFirstNumber);
     }
 }
