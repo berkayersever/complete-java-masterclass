a3246215228c2c3237c020721fdb9d5fdd6b667d
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-17 17:12:36 +0300

    Minor change.

--- ByteShortIntLong/src/Main.java
+++ ByteShortIntLong/src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Integer Minimum Value = " + myMinIntValue);
         System.out.println("Integer Maximum Value = " + myMaxIntValue);
         System.out.println("Busted Maximum Value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Minimum Value = " + (myMinIntValue - 1));
     }
 }
