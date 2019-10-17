6f04cf6f21e403d50ef05e5625a282310dce5a8a
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-17 16:58:23 +0300

    Minor changes.

--- ByteShortIntLong/src/academy/learnprogramming/Main.java
+++ ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         int myValue = 10000;
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
     }
 }
