81adf18fa491b131e1698dfba68a2d0c64f59b20
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-17 18:36:39 +0300

    Minor changes.

--- ByteShortIntLong/src/Main.java
+++ ByteShortIntLong/src/Main.java
@@ -21,5 +21,10 @@ public class Main {
         short myMaxShortValue = Short.MAX_VALUE;
         System.out.println("Short Minimum Value = " + myMinShortValue);
         System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
     }
 }
