7ba8e22dad7fd4a0d6d4dbccdf92b558c1546542
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-17 17:02:33 +0300

    Update Main.java

---
+++ ByteShortIntLong/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int myValue = 10000;
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+    }
+}
