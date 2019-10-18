2fcaeeef4392a5ef16e7656c0210a4a75874dce8
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-18 14:37:18 +0300

    Minor changes.

--- PrimitiveTypesChallenge/src/Main.java
+++ PrimitiveTypesChallenge/src/Main.java
@@ -9,5 +9,12 @@ public class Main {
         short shortTotal = (short) (1000 + 10 * (byteValue + shortValue + intValue));
         System.out.println("Short Total = " + shortTotal);
         System.out.println("Long Total = " + longTotal);
+
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00 / 3.00;
+        System.out.println("My Int Value = " + myIntValue);
+        System.out.println("My Float Value = " + myFloatValue);
+        System.out.println("My Double Value = " + myDoubleValue);
     }
 }
