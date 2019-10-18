6e66c8465bfddcb58007a7c7a79b60d302ed4ced
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-18 14:41:52 +0300

    Minor changes.

--- FloatAndDouble/src/Main.java
+++ FloatAndDouble/src/Main.java
@@ -12,8 +12,11 @@ public class Main {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+        System.out.println("My Int Value = " + myIntValue);
+        System.out.println("My Float Value = " + myFloatValue);
+        System.out.println("My Double Value = " + myDoubleValue);
     }
 }
