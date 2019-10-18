d5dd02ee2a7c5a3d62799736dbe5f0648f9e2871
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-18 17:39:30 +0300

    Char primitive type.

---
+++ CharAndBoolean/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+    }
+}
