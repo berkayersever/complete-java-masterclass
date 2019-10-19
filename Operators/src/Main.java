e7270f81a5d6a42b6831922daa288c947427d65c
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-19 21:05:37 +0300

    Minor changes.

--- Operators/src/Main.java
+++ Operators/src/Main.java
@@ -43,9 +43,14 @@ public class Main {
             System.out.println("And I am scared of aliens!");
         }
 
-        int topScore = 100;
-        if (topScore == 100) {
+        int topScore = 80;
+        if (topScore < 100) {
             System.out.println("You got the highest score!");
+        }
+
+        int secondTopScore = 90;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
         }
     }
 }
