3094c643784f657eee9ecdce5c096f3875be3d8b
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-19 15:31:06 +0300

    More examples.

--- Operators/src/Main.java
+++ Operators/src/Main.java
@@ -3,6 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         int result = 1 + 2;     // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("Previous result = " + previousResult);
+        result = result - 1;
+        System.out.println("3 - 1 = " + result);
+        System.out.println("Previous result = " + previousResult);
 
     }
 }
