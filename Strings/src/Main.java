72e661847241918a0af7baccbb49b79478b15627
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-19 14:49:47 +0300

    String examples.

--- Strings/src/Main.java
+++ Strings/src/Main.java
@@ -10,5 +10,16 @@ public class Main {
         // double
         // char
         // boolean
+
+        String myString = "This is a string";
+        System.out.println("My string is equal to '" + myString + "'");
+        myString += ", and this is more.";
+        System.out.println("My string is equal to '" + myString + "'");
+        myString += " \u00A92019";
+        System.out.println("My string is equal to '" + myString + "'");
+        String numberString = "250.55";
+        numberString += "49.95";
+        System.out.println(numberString);
+
     }
 }
