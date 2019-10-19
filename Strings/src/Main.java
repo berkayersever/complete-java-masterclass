712bf5afd106b1f4627833660b0a8e75207e35ce
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-19 14:39:25 +0300

    Eight primitive data types.

---
+++ Strings/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+    }
+}
