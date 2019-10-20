402eb6cc7fe5629debfd970a1dd0b08b0e44eaba
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-20 18:57:34 +0300

    Minor changes.

--- StatementsWhiteSpaceAndIndenting/src/Main.java
+++ StatementsWhiteSpaceAndIndenting/src/Main.java
@@ -7,5 +7,7 @@ public class Main {
         myVariable--;
         System.out.println("This is a test.");
         System.out.println("This is" + " another" + " still more.");
+
+        int anotherVariable = 50;anotherVariable--;System.out.println("This is another one");
     }
 }
