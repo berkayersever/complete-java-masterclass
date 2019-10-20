80dcf56cb7e03ce9235057fb984874837e692a3c
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-20 21:04:10 +0300

    Minor changes.

--- IfKeywordAndCodeBlocks/src/Main.java
+++ IfKeywordAndCodeBlocks/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        if (score <= 5000 && score > 1000) {
+        /*if (score <= 5000 && score > 1000) {
             System.out.println("Your score was less than 5000, but greater than 1000.");
         }
         else if (score < 1000) {
@@ -15,6 +15,16 @@ public class Main {
         }
         else {
             System.out.println("Got here!");
+        }*/
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+            score = 10000;
+            levelCompleted = 8;
+            bonus = 200;
+            finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
         }
     }
 }
