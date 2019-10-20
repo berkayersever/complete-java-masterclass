d14c57c75ed20879b47ab01e9f2e0ebccda66233
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-20 18:59:33 +0300

    Minor changes.

--- StatementsWhiteSpaceAndIndenting/src/Main.java
+++ StatementsWhiteSpaceAndIndenting/src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         int myVariable = 50;
+        if (myVariable == 50) {
+            System.out.println("Printed!");
+        }
         myVariable++;
         myVariable--;
         System.out.println("This is a test.");
