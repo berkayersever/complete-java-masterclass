0e0a504260775d7f0d9f19b521eb9deb77c75dad
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-21 22:58:33 +0300

    Minor changes.

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -6,12 +6,16 @@ public class Main {
         calculateScore(true, 10000, 8, 200);
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
+            finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
+            return finalScore;
+        }
+        else {
+            return -1;
         }
     }
 }
