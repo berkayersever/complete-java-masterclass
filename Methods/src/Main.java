952eb141e11ca79806d10c148c1401f4a9dac185
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-10-31 04:19:12 +0300

    Minor changes.

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -4,10 +4,40 @@ public class Main {
 
         calculateScore(true, 800, 5, 100);
         calculateScore(true, 10000, 8, 200);
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Bob", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Percy", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Gilbert", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Louise", highScorePosition);
     }
 
     public static void displayHighScorePosition(String playerName, int highScorePosition) {
         System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore > 1000) {
+            return 1;
+        }
+        else if (playerScore > 500 && playerScore < 1000) {
+            return 2;
+        }
+        else if (playerScore > 100 && playerScore < 500) {
+            return 3;
+        }
+        else {
+            return 4;
+        }
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
