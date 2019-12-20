a054153936c1df7b48740a85e4b7f026da1b7585
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-12-20 18:33:44 +0300

    Implemented the calculateFeetAndInchesToCentimeters methods.

--- MethodOverloading/src/Main.java
+++ MethodOverloading/src/Main.java
@@ -5,6 +5,11 @@ public class Main {
         System.out.println("New score is " + newScore);
         calculateScore(75);
         calculateScore();
+        double centimeters = calculateFeetAndInchesToCentimeters(6, 0);
+        if (centimeters < 0) {
+            System.out.println("Invalid parameters!");
+        }
+        calculateFeetAndInchesToCentimeters(157);
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -20,5 +25,25 @@ public class Main {
     public static int calculateScore() {
         System.out.println("No player name, no player score.");
         return 0;
+    }
+
+    public static double calculateFeetAndInchesToCentimeters(double feet, double inches) {
+        if (feet < 0 || inches < 0 || inches > 12) {
+            System.out.println("Invalid feet or inches parameter!");
+            return -1;
+        }
+        double centimeters = feet * 12 * 2.54 + inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
+        return centimeters;
+    }
+
+    public static double calculateFeetAndInchesToCentimeters(double inches) {
+        if (inches < 0) {
+            return -1;
+        }
+        double feet = (int) (inches / 12);
+        double remainder = (int) (inches % 12);
+        System.out.println(inches + " inches is equal to " + feet + " feet and " + remainder + " inches.");
+        return calculateFeetAndInchesToCentimeters(feet, remainder);
     }
 }
