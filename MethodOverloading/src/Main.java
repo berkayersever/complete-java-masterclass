69c5ce97ea3f8591dd2048a32bf83d8edd1de8ca
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-12-20 15:44:02 +0300

    Overloaded the calculateScore method.

--- MethodOverloading/src/Main.java
+++ MethodOverloading/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         int newScore = calculateScore("Berkay", 500);
         System.out.println("New score is " + newScore);
         calculateScore(75);
+        calculateScore();
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -14,5 +15,10 @@ public class Main {
     public static int calculateScore(int score) {
         System.out.println("Unnamed player scored " + score + " points.");
         return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name, no player score.");
+        return 0;
     }
 }
