4a7cd23239f90a6645938ef6cc6eabc993ef43cb
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-29 02:47:05 +0300

    Implemented the getDurationString method.

---
+++ SecondsAndMinutesChallenge/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getDurationString(65, 45));
+    }
+
+    private static String getDurationString(int minutes, int seconds) {
+        if ((minutes < 0) || (seconds < 0) || (seconds > 59)) {
+            return "Invalid Value";
+        }
+        int hours = minutes / 60;
+        int remainingMinutes = minutes % 60;
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+    }
+}
