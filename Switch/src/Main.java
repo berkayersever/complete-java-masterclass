ec98ea7e5c650ad1ab9606b0721bfac63c0b6e46
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-30 16:21:33 +0300

    Update Main.java

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -11,5 +11,19 @@ public class Main {
         else {
             System.out.println("Value was not 1 or 2");
         }
+
+        int switchValue = 1;
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.println("Value was not 1 or 2");
+                break;
+        }
+        // More code to be executed after the the break statement
     }
 }
