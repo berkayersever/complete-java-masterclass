4827c8d2eb82ee26358a8f8bbf7de45318e4c06d
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-31 02:58:31 +0300

    Implemented printDayOfTheWeek method using switch and if...else.

--- DayOfTheWeekChallenge/src/Main.java
+++ DayOfTheWeekChallenge/src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
 
+    private static final String[] DAYS = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    private static final String INVALID_DAY = "Invalid Day";
+
     public static void main(String[] args) {
-	// write your code here
+        for (int i = 0; i < 8; i++) {
+            printDayOfTheWeek(i);
+        }
+    }
+
+    private static void printDayOfTheWeek(int day) {
+        switch (day) {
+            case 0: case 1: case 2: case 3: case 4: case 5: case 6:
+                System.out.println("Using switch\t:\t" + DAYS[day]);
+                break;
+            default:
+                System.out.println("Using switch\t:\t" + INVALID_DAY);
+                break;
+        }
+        if (0 <= day && day <= 6) {
+            System.out.println("Using if...else\t:\t" + DAYS[day]);
+        }
+        else {
+            System.out.println("Using if...else\t:\t" + INVALID_DAY);
+        }
     }
 }
