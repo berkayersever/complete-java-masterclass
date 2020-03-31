7c82c873982c8e9e55b9925a09dbeb3391bbea96
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-01 02:39:53 +0300

    Update Main.java

--- ForLoop/src/Main.java
+++ ForLoop/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("10.000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+        for (int i = 1; i < 10; i++) {
+            System.out.printf("$10.000 at %d%% interest is $%.1f %n", i, calculateInterest(10000.0, i));
+        }
     }
 
     public static double calculateInterest(double amount, double interestRate) {
