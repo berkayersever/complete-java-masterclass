1435d4764510662d6347c0f30c91b4c77c729d83
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-01 22:40:08 +0300

    Reverted the order of the for loop.

--- ForLoop/src/Main.java
+++ ForLoop/src/Main.java
@@ -2,7 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
-            // System.out.printf("$10.000 at %d%% interest is $%.1f %n", i, calculateInterest(10000.0, i));
+            /* System.out.printf("$10.000 at %d%% interest is $%.1f %n", i, calculateInterest(10000.0, i)); */
+            System.out.println("$10.000 at " + i + "% interest is $" + String.format("%.1f", calculateInterest(10000, i)));
+        }
+        for (int i = 9; i > 0 ; i--) {
             System.out.println("$10.000 at " + i + "% interest is $" + String.format("%.1f", calculateInterest(10000, i)));
         }
     }
