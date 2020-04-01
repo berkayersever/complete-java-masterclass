9a54b0fd4c0750eb1fa01d72f495f8ce53110691
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-01 22:55:52 +0300

    Implemented the isPrime method.

--- ForLoop/src/Main.java
+++ ForLoop/src/Main.java
@@ -9,6 +9,26 @@ public class Main {
         for (int i = 9; i > 0 ; i--) {
             System.out.println("$10.000 at " + i + "% interest is $" + String.format("%.1f", calculateInterest(10000, i)));
         }
+        System.out.println("********************************");
+        int count = 0;
+        for (int i = 20; i < 100 && count < 3; i++) {
+            if (isPrime(i)) {
+                System.out.println("Number " + i + " is a prime number!");
+                count++;
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= n/2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static double calculateInterest(double amount, double interestRate) {
