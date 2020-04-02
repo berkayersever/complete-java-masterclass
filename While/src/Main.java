8a23009c0597776adf8523c87aebadb57ba88bef
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-02 05:17:57 +0300

    Update Main.java

--- While/src/Main.java
+++ While/src/Main.java
@@ -2,13 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
         int count = 1;
-        while (count != 6) {
+        while(count != 6) {
             System.out.println("Count: " + count);
             count++;
         }
         System.out.println("********");
         for (count = 1; count != 6; count++) {
             System.out.println("Count: " + count);
+        }
+        System.out.println("********");
+        count = 1;
+        while(true) {
+            if(count == 6) {
+                break;
+            }
+            System.out.println("Count: " + count);
+            count++;
         }
     }
 }
