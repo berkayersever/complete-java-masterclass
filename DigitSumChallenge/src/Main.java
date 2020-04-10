cbbb5aeb871638cb294cc26e24d5f03b814440fb
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-10 23:03:07 +0300

    Update Main.java

--- DigitSumChallenge/src/Main.java
+++ DigitSumChallenge/src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.printf("Sum of the digits in number %d: " + sumDigits(125) + "\n", 125);
+        System.out.printf("Sum of the digits in number %d: " + sumDigits(-125) + "\n", -125);
+        System.out.printf("Sum of the digits in number %d :" + sumDigits(4) + "\n", 4);
+        System.out.printf("Sum of the digits in number %d: " + sumDigits(32123) + "\n", 32123);
+    }
+
+    private static int sumDigits(int number) {
+
+        System.out.print("");
+        if(number < 10) {
+            return -1;
+        }
+        int sum = 0;
+        while(number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
     }
 }
