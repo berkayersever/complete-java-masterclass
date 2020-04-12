92962fd5fbd7a6b4827389188da124645cf75e8f
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-12 14:10:54 +0300

    Update Main.java

--- ParsingValuesFromString/src/Main.java
+++ ParsingValuesFromString/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String numberAsString = "2018";
+        System.out.println("numberAsString = " + numberAsString);
     }
 }
