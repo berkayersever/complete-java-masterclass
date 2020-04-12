1da84e2b41658f2b78edf82067b7e00b38b3a4dd
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-12 14:27:21 +0300

    Update Main.java

--- ParsingValuesFromString/src/Main.java
+++ ParsingValuesFromString/src/Main.java
@@ -6,5 +6,11 @@ public class Main {
 
         int number = Integer.parseInt(numberAsString);
         System.out.println("Number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        System.out.println("Number = " + number);
     }
 }
