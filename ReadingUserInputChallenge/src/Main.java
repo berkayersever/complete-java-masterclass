11e4817d3515ee577811cf6901b631e4a4381f13
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-14 23:46:06 +0300

    Update Main.java

--- ReadingUserInputChallenge/src/Main.java
+++ ReadingUserInputChallenge/src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int counter = 1, sum = 0;
+        while(counter <= 10) {
+            System.out.println("Enter number #" + counter + ":");
+            boolean hasNextInt = scanner.hasNextInt();
+            if(hasNextInt) {
+                int number = scanner.nextInt();
+                counter++;
+                sum += number;
+            }
+            else {
+                System.out.println("Invalid Number");
+            }
+            scanner.nextLine();     // Handle the next line character (Enter Key)
+        }
+        System.out.println("Sum of the numbers = " + sum);
+        scanner.close();
     }
 }
