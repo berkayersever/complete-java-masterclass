cb2d17f1776e54543f1aa2b52c955c002fe9fbc1
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-16 23:36:17 +0300

    Update Main.java

--- Classes/src/Main.java
+++ Classes/src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+
         Car porsche = new Car();
         Car mustang = new Car();
         porsche.setModelYear(2020);
+        System.out.println("Model year is " + porsche.getModelYear());
     }
 }
