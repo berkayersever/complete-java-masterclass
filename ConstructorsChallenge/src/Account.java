dcaa14f7badeb5fba900b6cbf191aa9176739741
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-18 23:47:31 +0300

    Implemented the getter methods for the Account class.

--- ConstructorsChallenge/src/Account.java
+++ ConstructorsChallenge/src/Account.java
@@ -1,2 +1,28 @@
 public class Account {
+
+    private int accountNumber;
+    private int balance;
+    private int phoneNumber;
+    private String customerName;
+    private String email;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
