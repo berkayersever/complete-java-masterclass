f82b3b0f13425e0a4c6bbd38c6fb7cb01c4cdcee
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-19 21:49:41 +0300

    Update VipCustomer.java

--- ConstructorsChallenge/src/VipCustomer.java
+++ ConstructorsChallenge/src/VipCustomer.java
@@ -1,2 +1,32 @@
 public class VipCustomer {
+
+    private String name;
+    private int creditLimit;
+    private String emailAddress;
+
+    public VipCustomer() {
+        this("John", 5000, "john@doe.com");
+    }
+
+    public VipCustomer(String name, String emailAddress) {
+        this(name, 2000, emailAddress);
+    }
+
+    public VipCustomer(String name, int creditLimit, String emailAddress) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
 }
