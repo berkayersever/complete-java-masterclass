c30b0b4eea5b3f395a7bf62c596ebffd4162890c
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-20 02:58:33 +0300

    Update Dog.java

--- Inheritance/src/Dog.java
+++ Inheritance/src/Dog.java
@@ -28,9 +28,11 @@ public class Dog extends Animal {
 
     public void walk() {
         System.out.println("Dog.walk() is called!");
+        move(5);
     }
 
     public void run() {
         System.out.println("Dog.run() is called!");
+        move(10);
     }
 }
