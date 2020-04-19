97ec9495ea2384b04b2fdf4286e2d4fab4e67bd6
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-19 05:04:26 +0300

    Update Main.java

--- ConstructorsChallenge/src/Main.java
+++ ConstructorsChallenge/src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Account myAccount = new Account();
+        myAccount.setNumber("123456789");
+        myAccount.setBalance(0);
+        myAccount.setCustomerName("John Doe");
+        myAccount.setCustomerEmail("john@doe.com");
+        myAccount.setCustomerPhone("(532) 987-65-43");
+
+        myAccount.withdraw(100.0);
+        myAccount.deposit(50.0);
+        myAccount.withdraw(100.0);
+
+        myAccount.deposit(51.0);
+        myAccount.withdraw(100.0);
     }
 }
