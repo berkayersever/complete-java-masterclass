d29c7c9d1c450984e405e3944e4f378e7f35e810
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-19 05:12:37 +0300

    Update Main.java

--- ConstructorsChallenge/src/Main.java
+++ ConstructorsChallenge/src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         Account myAccount = new Account("123456789", 0.0, "John Doe", "john@doe.com", "(532) 987-65-43");
+        System.out.println(myAccount.getNumber());
+        System.out.println(myAccount.getBalance());
         /*myAccount.setNumber("123456789");*/
         /*myAccount.setBalance(0);*/
         /*myAccount.setCustomerName("John Doe");*/
