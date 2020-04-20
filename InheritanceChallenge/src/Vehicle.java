c5cdf7f9cc8653752c10a3d1d517bded0bbee63b
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-21 02:48:06 +0300

    Update Vehicle.java

--- InheritanceChallenge/src/Vehicle.java
+++ InheritanceChallenge/src/Vehicle.java
@@ -1,2 +1,14 @@
 public class Vehicle {
+
+    private String name;
+    private String size;
+    private int currentVelocity;
+    private int currentDirection;
+
+    public Vehicle(String name, String size) {
+        this.name = name;
+        this.size = size;
+        this.currentVelocity = 0;
+        this.currentDirection = 0;
+    }
 }
