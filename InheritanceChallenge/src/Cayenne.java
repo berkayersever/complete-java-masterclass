38bb5bef45c2bfb622975db52de2bdd94a1ff4dd
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-21 03:21:37 +0300

    Update Cayenne.java

--- InheritanceChallenge/src/Cayenne.java
+++ InheritanceChallenge/src/Cayenne.java
@@ -1,2 +1,9 @@
-public class Cayenne {
+public class Cayenne  extends Car {
+
+    private int roadServiceMonths;
+
+    public Cayenne(int roadServiceMonths) {
+        super("Cayenne", "4WD", 5, 5, 6, false);
+        this.roadServiceMonths = roadServiceMonths;
+    }
 }
