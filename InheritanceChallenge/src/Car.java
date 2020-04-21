4644b5671580f0c9c636dbb78d67db8feb31950a
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-21 03:07:14 +0300

    Update Car.java

--- InheritanceChallenge/src/Car.java
+++ InheritanceChallenge/src/Car.java
@@ -14,4 +14,9 @@ public class Car extends Vehicle {
         this.isManual = isManual;
         this.currentGear = 1;
     }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+        System.out.println("Car.setCurrentGear(): Changed to " + this.currentGear + " gear.");
+    }
 }
