7d75ae2f67ae5d3ce816cedece81f109c9d25067
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-21 03:18:00 +0300

    Update Carrera.java

--- InheritanceChallenge/src/Carrera.java
+++ InheritanceChallenge/src/Carrera.java
@@ -1,2 +1,9 @@
-public class Carrera {
+public class Carrera extends Car {
+
+    private int roadServiceMonths;
+
+    public Carrera(String name, String size, int wheels, int doors, int gears, boolean isManual, int roadServiceMonths) {
+        super("Carrera", size, wheels, doors, gears, isManual);
+        this.roadServiceMonths = roadServiceMonths;
+    }
 }
