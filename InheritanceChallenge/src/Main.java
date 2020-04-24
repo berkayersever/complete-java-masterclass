7555813bc2e31a30a96d859602a62a551ba17f2a
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-04-25 00:34:40 +0300

    Update Main.java

--- InheritanceChallenge/src/Main.java
+++ InheritanceChallenge/src/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         Cayenne cayenne = new Cayenne(36);
+        cayenne.steer(45);
         cayenne.accelerate(30);
         cayenne.accelerate(20);
+        cayenne.accelerate(-45);
     }
 }
